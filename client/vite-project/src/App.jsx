bc49db699162e01b0d4e1e7972155f84c9aa187a
baskaranjeeva <baskaran.jeevanandham@gmail.com>
2025-06-24 10:36:35 +0530

    Initial design and setup ready

---
+++ client/vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/vite-project/src/pages/Register.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+import "./auth.css"; // Importing CSS for consistency
+import { Link } from "react-router-dom";
+
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+function Register() {
+  return (
+    <div>
+      <div className="auth-container">
+        <div className="auth-box">
+          <h1 className="title">Register for MovieBook</h1>
+          <Form
+            name="basic"
+            layout="vertical"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label={<span style={{ color: "white" }}>Name</span>}
+              name="name"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input
+                type="text"
+                placeholder="Enter your name"
+                className="input-field"
+              />
+            </Form.Item>
+            <Form.Item
+              label={<span style={{ color: "white" }}>Email</span>}
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your email to proceed",
+                },
+              ]}
+            >
+              <Input
+                type="email"
+                placeholder="Enter your email"
+                className="input-field"
+              />
+            </Form.Item>
+            <Form.Item
+              label={<span style={{ color: "white" }}>Password</span>}
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input
+                type="password"
+                placeholder="Enter your password"
+                className="input-field"
+              />
+            </Form.Item>
+
+            <Button
+              type="primary"
+              block
+              htmlType="submit"
+              className="auth-button"
+            >
+              Register
+            </Button>
+          </Form>
+          <p className="switch-text">
+            Already a user? New User?
+            <Link to="/login"> Login here</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
